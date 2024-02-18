48431ae7081c199a5a939f2da45f08a08ca94d86
lamonica-d <dominique.lamonica@gmail.com>
2024-02-18 17:28:23 +0100

    add paths

--- R/paths.R
+++ R/paths.R
@@ -1,9 +1,9 @@
 
 options(Encoding="latin1")
-# encoding en latin1 pour l'accent de Aurélien
+# encoding en latin1 pour l'accent de Aur?lien
 
-################################ Chemins Aurélien
-if(Sys.info()[['effective_user']] == "Aurélien"){
+################################ Chemins Aur?lien
+if(Sys.info()[['effective_user']] == "Aur?lien"){
   
   path_to_Documents = file.path(
     "C:",
@@ -44,7 +44,10 @@ if(Sys.info()[['effective_user']] == "lamonica"){
     )
 
     path_to_Forest_savanna_project_folder = file.path(
-      "à compléter",
+      path_to_Documents,
+      "dessfor",
+      "stage_aurelien_thiriet",
+      "Forest_savanna_project"
     )
     
     path_to_Savanna_structure_GEDI_folder = file.path(
@@ -55,8 +58,9 @@ if(Sys.info()[['effective_user']] == "lamonica"){
     )
     
     path_to_GEDI_raw_data = file.path(
-      "le chemin vers le fichier total
-      (pas le rawdata à l'intérieur du folder github),
-      pour ne pas avoir à push toutes les données sur github"
+      path_to_Documents,
+      "dessfor",
+      "gedi_data_lebien",
+      "pix_extract"
     )
 }
